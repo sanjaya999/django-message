8309a33c51aca6d62b3a1523d86fafa5a3c7ab8c
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-16 09:24:43 +0545

    add axios

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,60 @@
+import axios from "axios"
+
+//base axios instance created with base url and headers (default)
+const apiClient = axios.create({
+    baseURL: "http://localhost:9000",
+    headers : {
+        'Content-Type': 'application/json',
+    }
+})
+
+//interceptor will add authorization headers to every request
+apiClient.interceptors.request.use(
+    config =>{
+        const token = localStorage.getItem('authToken');
+        if(token){
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error =>{
+        return Promise.reject(error)
+    }
+)
+
+//get request
+export const get = async(endpoint) =>{
+    try{
+        const response = await apiClient.get(endpoint);
+        return response;
+    }catch(error){
+        console.error('Error fetching data:', error);
+        throw error;        
+    }
+
+}
+ 
+//post request
+export const post = async(endpoint, data)=>{
+    try{
+        const response = await apiClient.post(endpoint , data);
+        return response;
+    }catch(error){
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+//delete req
+
+export const del = async(endpoint)=>{
+    try{
+        const response = await apiClient.delete(endpoint);
+        return response;
+    }catch(error){
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+export {apiClient}
--- Frontend/src/component/Login/Register.jsx
+++ Frontend/src/component/Login/Register.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React ,{useState}from "react";
 
 function Register(){
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
 
+    const handleChange = (e)=>{
+        const {name , value } = e.target
+        setData({
+            ...data,
+            [name] : value
+        })
+        
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        // API call for register
+    }
+    
     return(
         <>
-            this is register page
+            <h1>Register</h1>
+
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" 
+                name="name"
+                placeholder="fullname" 
+                value = {data.name}
+                onChange={handleChange}
+                />
+
+                <input type="text" 
+                placeholder="email"
+                name="email"
+                value={data.username}
+                onChange={handleChange}
+                 />
+
+                <input type="password"
+                 placeholder="Password"
+                 name="password"
+                 value={data.password}
+                 onChange={handleChange}
+                 />
+                
+                <input type="submit" value="Register" />
+            </form>
+
         </>
     )
 }
