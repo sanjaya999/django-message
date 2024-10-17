2804d65ea0345559064e8a2920cab21e845b6e43
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-17 08:56:37 +0545

    alert message added

--- Frontend/src/component/Login/Register.jsx
+++ Frontend/src/component/Login/Register.jsx
@@ -4,6 +4,7 @@ import { post } from "../../api/api";
 
 
 function Register(){
+    const[alertMessage , setAlertMessage] = useState("")
     const [data, setData] = useState({
         fullname: "",
         email: "",
@@ -18,40 +19,66 @@ function Register(){
         })
         
     }
-    const handleSubmit = async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await post(`/register` , data);
-        console.log(response);
-    }
-
+    
+        try {
+            const response = await post(`/register`, data)
+            //display successful alert message
+            if (response.status === "201") {
+                setAlertMessage("Registration Successful");
+            }
+        } catch (error) {
+            //alert on email already exist message
+            const errorMessage = error.response.data.message || "";
+            if(errorMessage.includes("email already exists")){
+                setAlertMessage("Email already exists");
+            }else {
+                setAlertMessage("Registration Failed");
+            }
+        }
+    };
     return(
         <>
         <div className="container">
+        {alertMessage && (
+                <div className="alert">
+                    {alertMessage}
+                </div>
+            )}
         <div className="register">
+           
             <h1>Register</h1>
 
             <form action="" className="registerForm"
             onSubmit={handleSubmit}>
+                <div className="inner">
                 <input type="text" 
+                className="login-input"
                 name="fullname"
                 placeholder="fullname" 
                 value = {data.name}
-                onChange={handleChange}
+                onChange={handleChange} required
                 />
+                </div>
 
+                <div className="inner">
                 <input type="text" 
+                 className="login-input"
                 placeholder="email"
                 name="email"
                 value={data.username}
-                onChange={handleChange}
-                 />
+                onChange={handleChange} required
+                 /></div>
 
+                <div className="inner">
                 <input type="password"
+                 className="login-input"
                  placeholder="Password"
                  name="password"
                  value={data.password}
-                 onChange={handleChange}
-                 />
+                 onChange={handleChange} required
+                 /></div>
                 
                 <input type="submit" value="Register" />
                 <h3>Doesnt have an account ? 
