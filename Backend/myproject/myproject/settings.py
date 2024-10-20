a527fe593794a1b14511b294bb6304b7d1012b00
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-20 05:55:43 +0545

    jwt added

--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -53,6 +53,14 @@ REST_FRAMEWORK = {
     ],
 }
 
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
+
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
