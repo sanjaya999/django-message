1f8eb097dec00f0535fadbde4350840278ef6850
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-21 08:54:55 +0545

    models added

--- Backend/myproject/myapp/admin.py
+++ Backend/myproject/myapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models.usermodel import CustomUser
+from .models.usermodel import CustomUser , conversation , Message
 # Register your models here.
 
 admin.site.register(CustomUser)
+
+admin.site.register(conversation)
+admin.site.register(Message)
--- Backend/myproject/myapp/models/usermodel.py
+++ Backend/myproject/myapp/models/usermodel.py
@@ -10,3 +10,25 @@ class CustomUser(AbstractUser):
   
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []  
+
+class conversation(models.Model):
+    participants = models.ManyToManyField(CustomUser , related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Conversation {self.id} between {', '.join([user.username for user in self.participants.all()])}"
+    
+
+class Message(models.Model):
+    conversation = models.ForeignKey(conversation, on_delete=models.CASCADE , related_name="messages")
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="sent_messages")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message {self.id} from {self.sender.username} in Conversation {self.conversation.id}"
+
+
