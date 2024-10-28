611f90bcbb3cced703a5e268e11615e67f9df3cb
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-28 08:02:53 +0545

    admin added

--- Backend/myproject/myapp/admin.py
+++ Backend/myproject/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models.usermodel import CustomUser 
+from .models.usermodel import CustomUser , conversation
 
 admin.site.register(CustomUser)
 
+admin.site.register(conversation)
