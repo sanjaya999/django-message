1dc6b726ae68aa7ff2fac125873801f121082f5a
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-31 12:07:48 +0545

    user added

--- Frontend/src/component/User.jsx
+++ Frontend/src/component/User.jsx
@@ -1,6 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { get } from "../api/api"
+import { convertToRelativeTime } from "../functions/time"
 
-function User (){
+function User ({userId ,  onSelectConvo ,onSideSelect}){
+    const [conversation , setconversation] = useState([])
+    
+    
+    useEffect(()=>{
+        
+        getconversation();
+    },[userId])
+
+    const getconversation =async()=>{
+        try {
+            const response = await get(`get-user-conversations`)
+            setconversation(response)
+            
+        } catch (error) {
+                console.log(error)
+        }
+    }
+
+    const selectConvo = (conversationId , otherUserId)=>{
+        onSelectConvo(conversationId)
+        onSideSelect(otherUserId)
+    }
 
 
 
@@ -8,8 +32,16 @@ function User (){
     return(
 
         <>
-        <div>
-            user
+        <div className="conversations-container">
+            {conversation?.map((convo) => (
+                <div key={convo.conversation_id} 
+                onClick={()=>selectConvo(convo.conversation_id , convo.other_user.id)}
+                className="conversation-card"> 
+                    <div className="user-name">{convo.other_user.fullname}</div>
+                    <div className="message-content">{convo.last_message.content}</div>
+                    <div className="timestamp">{convertToRelativeTime(convo.last_message.timestamp)}</div>
+                </div>
+            ))}
         </div>
         </>
     )
