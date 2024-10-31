87cece6afc61a5433ece344a22de619d5c24e7fe
Sanjaya <poudelsanjaya12@gmail.com>
2024-10-31 12:07:57 +0545

    fetch message added

--- Frontend/src/component/Message.jsx
+++ Frontend/src/component/Message.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { get, post } from '../api/api'
 
-function Message() {
+function Message({conversationId ,otherUser}) {
+  const currentUser = localStorage.getItem("user_id")
+
+  const [convoFound , setconvoFound] = useState(null)
+  const [currentConvo , setcurrentConvo] = useState("")
+  const [Message , setMessage] = useState("")
+
+  useEffect(()=>{
+    //check if conversation exist or not and create new  if not
+    const get_create_convo =async()=>{
+      try {
+        const response = await post(`/conversations`,
+          {
+            "user_id": `${currentUser}`,
+             "user" : `${otherUser}`
+          }
+        )
+        console.log("check convo" , response)
+        if(response.message.includes("Existing conversation found.")){
+          setconvoFound(true)
+          setMessage(response)
+        }else{
+          setcurrentConvo(response.conversation_id)
+        }
+      
+      } catch (error) {
+        console.log(error)
+        
+      }
+    }
+    get_create_convo();
+   
+  },[conversationId])
+
+  //fetch conversation
+  useEffect(()=>{
+    const fetchMessage =async ()=>{
+      try {
+        const response = await get(`getmessage/${conversationId}`)
+        console.log("these are messages" , response.results);
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMessage();
+  },[conversationId])
   return (
-    <div>Message</div>
+
+    <div>Message {conversationId}</div>
   )
 }
 
