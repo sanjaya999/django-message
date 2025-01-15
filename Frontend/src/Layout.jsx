869f31fa1127ca70c497696b979b099b87dc12a9
San <poudelsanjaya12@gmail.com>
2025-01-15 12:27:17 +0545

    removed props and added redux

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -4,54 +4,32 @@ import Search from './component/Search';
 import User from './component/User';
 import Message from './component/Message';
 import Nav from './component/Nav';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedUser,setSelectConv, setMessageUser } from './features/layoutSlice';
+
 
 
 
 
 
 const Layout = () => {
-  const[selectedUser , setSelectedUser] = useState(null)
-  const [selectConv , setselectConv] = useState(null)
-  const [messageUser , setmessageUser] = useState(null)
 
-  const handleUserSelect = (userId) => {
-    console.log("Layout: New user selected:", userId);
-    setSelectedUser(userId);
+
   
-    // Only reset states if selecting a different user
-    if (userId !== selectedUser) {
-      setmessageUser(null);
-      setselectConv(null);
-    }
-  };
-  
-
-  const handleConvo = (conversationId)=>{
-    setselectConv(conversationId)
-  }
-
-  const MessageUserSelect = (otherUserId)=>{
-    setmessageUser(otherUserId)
-  }
   return (
     <div>
       <Nav />
       <div className="body">
         <div className="userArea">
-          <Search  onSelectUser = {handleUserSelect}/>
-          <User userId={selectedUser}
-          onSelectConvo={handleConvo}
-          onSideSelect={MessageUserSelect}/>
+          <Search  />
+          <User />
         </div>
 
-      {selectConv && messageUser && (
+     
         <div className="messageArea">
-        <Message
-         key={`${selectConv}-${messageUser}`}
-         conversationId={selectConv}
-         otherUser={messageUser} />
+        <Message />
       </div>
-      )}
+      
         
       </div>
     </div>
