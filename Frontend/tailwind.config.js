37a1d3b11fdb17a01778a7e6b2d6bc682c267a56
San <poudelsanjaya12@gmail.com>
2025-01-18 11:07:33 +0545

    tailwind config added

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default{
+  content: [
+    './index.html',   // Include your root HTML file
+    './src/**/*.{js,ts,jsx,tsx}', // Include your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
