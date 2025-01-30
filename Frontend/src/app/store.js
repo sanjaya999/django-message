c2df014e274497c81e56c83539221a3d967aaba3
San <poudelsanjaya12@gmail.com>
2025-01-31 01:49:07 +0545

    Rtc redux

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import layoutReducer from "../features/layoutSlice"
+import callReducer from "../features/callSlice"
 
 const store = configureStore({
     reducer:{
         Layout: layoutReducer,
+        call : callReducer,
     }
 })
 
