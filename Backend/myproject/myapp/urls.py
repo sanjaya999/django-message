4758a14f0edb832649e9832d102a7d9f3792f060
Sanjaya <poudelsanjaya12@gmail.com>
2025-02-12 17:56:17 +0545

    remove update_public_key endpoint from URLs; clean up unused routes

--- Backend/myproject/myapp/urls.py
+++ Backend/myproject/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import index , test_view  # Import specific view functions
-from .views import user_registration , login ,search_users, get_or_create_conversation,post_message,get_messages,get_user_conversations,update_public_key
+from .views import user_registration , login ,search_users, get_or_create_conversation,post_message,get_messages,get_user_conversations
 
 urlpatterns = [
     path('', index, name='index'),
@@ -14,7 +14,6 @@ urlpatterns = [
     path('message', post_message, name='post_message'),
     path('getmessage/<str:conversation_id>', get_messages, name='get_message'),
     path('get-user-conversations', get_user_conversations, name='get-user-conversations'),
-    path('key' , update_public_key , name="update_public_key")
 
 
 
